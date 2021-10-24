954f6da151f84db7735850c4aa911f633714515a
VadZim <vadzim11@tut.by>
2021-10-24 03:24:01 +0200

    vork version

---
+++ force-app/force-app/main/default/lwc/selectorAccountMonth/selectorAccountMonth.js
@@ -0,0 +1,105 @@
+import { LightningElement, track,  wire } from 'lwc';
+import getAccounts from '@salesforce/apex/SelectorAccountMonthController.getAccounts';
+import getMonths from '@salesforce/apex/SelectorAccountMonthController.getMonths';
+import getOrder from '@salesforce/apex/DatatableOrderController.getOrder';
+import getAccountOrder from '@salesforce/apex/DatatableOrderController.getAccountOrder';
+import getAccountMonthsOrder from '@salesforce/apex/DatatableOrderController.getAccountMonthsOrder';
+
+const COLUMNS = [
+    {label:'Order Name', fieldName:'Name'},
+    {label:'Account', fieldName:'Account__c'},
+    {label:'ITotal Amount', fieldName:'Total_Amount__c', type:'number'},
+    {label:'Payment Due date', fieldName:'Payment_Due_date__c', type:'date'},
+]
+
+export default class SelectorAccountMonth extends LightningElement {
+    columns = COLUMNS;
+
+    accountList;
+    montsList;
+
+    @track selectedAccount;
+    @track selectedMonths;
+
+    tableData;
+    tableAccountData;
+    tableAccountMonthsData;
+           
+    @wire(getAccounts) 
+    retrieveAccounts({data, error}){
+        let accountArray = new Array();
+        if(data){
+            for(let key in data){
+                accountArray.push({label:data[key],value:key});
+            }
+            if(error){
+                console.error(error);
+            }
+        }
+        this.accountList=accountArray;
+    }
+
+    @wire(getMonths, { selectedAccount: '$selectedAccount' }) 
+    retrieveMounths({data, error}){
+        let monthsArray = new Array();
+        if(data){
+            for(let key in data){
+                monthsArray.push({label:data[key],value:data[key]});
+            }
+            if(error){
+                console.error(error);
+            }
+        }
+        this.montsList=monthsArray;
+    }
+
+    @wire(getOrder)
+    ordersHandler({data, error}){ 
+        if(data){            
+            this.tableData = data;
+            console.log(this.tableData);
+        }
+        if(error){
+            console.error(error);
+        }
+    }
+
+    @wire(getAccountOrder, { selectedAccount: '$selectedAccount' })
+    ordersAccountHandler({data, error}){
+        if(data){
+            this.tableAccountData = data;
+            console.log(this.tableAccountData);
+            console.log(this.selectedAccount);
+        }
+        if(error){
+            console.error(error);
+        }
+    }
+
+    @wire(getAccountMonthsOrder, {
+        selectedAccount: '$selectedAccount', 
+        selectedMonths: '$selectedMonths' })
+    ordersAccountMonthsHandler({data, error}){
+        if(data){
+            this.tableAccountMonthsData = data;
+            console.log(this.tableAccountMonthsData);
+            console.log(this.selectedAccount);
+            console.log(this.selectedMonths);
+        }
+        if(error){
+            console.error(error);
+        }
+    }
+
+    /*hadnling*/
+    handleAccountChange(event){
+        this.selectedAccount = event?.detail && event?.detail?.value;
+        console.log(this.selectedAccount);
+    }
+
+    handleMonthsChange(event){
+        this.selectedMonths = event?.detail && event?.detail?.value;
+        console.log(this.selectedMonths);
+    }
+    
+}
